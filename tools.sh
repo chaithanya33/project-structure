f4a6aae7b167cebeee76f65ca65a2434bda2d1b8
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2025-11-25 08:07:08 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,1 +1,16 @@
 
+sudo yum install java-21-amazon-corretto -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-21-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+
+yum install git docker -y
+
+systemctl start docker
+systemctl start jenkins
